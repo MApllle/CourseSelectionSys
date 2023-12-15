1e96f630a066a14d8904d3f246e6fc80381d965c
MAple <1952972500@qq.com>
2023-12-15 13:28:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# 鼠鼠选课系统
+## 环境
+后端：
+- python:3.8.xx
+- Django 4.2.8
+- mysqlclient 2.2.1
+
+前端：
+- node v16.13.2
+- npm 16.13.2
+- [前端模板](https://github.com/PanJiaChen/vue-admin-template)
+
+## 功能
+登录，进入学期选择页面
+左侧显示：登录人信息（学生：学号、姓名、平均绩点；）
+1. 管理员
+    - 学生管理
+    - 教师管理
+    - (院系管理)
+    - 课程管理
+    - 选课管理
+2. 学生选课模块
+    - 选课
+    - 退课
+    - 课表查询
+    - (删除选课查询)
+    - 课程查询
+    - (选课排名查询)
+3. 教师
+    - 查询课表
+    - 增删改成绩
+
+## 项目结构
+后端：
+```
+│  manage.py    #用于启动django后端
+│
+├─backend       #后端的一些设置存放的目录
+│  │  asgi.py
+│  │  settings.py
+│  │  urls.py
+│  │  wsgi.py
+│  │  __init__.py
+│  │
+│  └─__pycache__
+│
+└─managementApp     #一个APP
+    │  admin.py
+    │  apps.py
+    │  models.py       #模型层 对应数据库里的表
+    │  tests.py
+    │  views.py     #写与后端交互的接口
+    │  __init__.py
+    │  
+    └─migrations
+            __init__.py
+```
+前端：
+## 开发记录
+
+## 参考资料
+官方文档 https://docs.djangoproject.com/zh-hans/4.2/
+快速上手 https://www.jianshu.com/p/0123d11f9609
---
+++ frontend/src/views/studentPage/selectCourse.vue
@@ -0,0 +1,122 @@
+<!--选课页面-->
+<template>
+    <div class="app-container">
+        <div class="inputboxes">
+            课程号：<el-input v-model="input" class="boxes" size="small"></el-input>
+            课程名：<el-input v-model="input" class="boxes" size="small"></el-input>
+            教师名：<el-input v-model="input" class="boxes" size="small"></el-input>
+            教师号：<el-input v-model="input" class="boxes" size="small"></el-input>
+        </div>
+        <div class="buttons">
+            <el-button type="primary" size="small">查询</el-button>
+        </div>
+        <el-divider></el-divider>
+      <el-table
+        v-loading="listLoading"
+        :data="list"
+        element-loading-text="Loading"
+        border
+        fit
+        highlight-current-row
+      >
+      <el-table-column label="勾选">
+          <template slot-scope="scope">
+            {{ scope.row.title }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="序号" width="95">
+          <template slot-scope="scope">
+            {{ scope.$index }}
+          </template>
+        </el-table-column>
+        <el-table-column label="课程号" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.author }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="课程名" width="110" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.author }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="学分" width="110" align="center">
+          <template slot-scope="scope">
+            {{ scope.row.pageviews }}
+          </template>
+        </el-table-column>
+        <el-table-column label="教师号" width="110" align="center">
+          <template slot-scope="scope">
+            {{ scope.row.pageviews }}
+          </template>
+        </el-table-column>
+        <el-table-column label="教师名" width="110" align="center">
+          <template slot-scope="scope">
+            {{ scope.row.pageviews }}
+          </template>
+        </el-table-column>
+        <el-table-column label="课程容量" width="110" align="center">
+          <template slot-scope="scope">
+            {{ scope.row.pageviews }}
+          </template>
+        </el-table-column>
+        <el-table-column label="已选课人数" width="110" align="center">
+          <template slot-scope="scope">
+            {{ scope.row.pageviews }}
+          </template>
+        </el-table-column>
+        <el-table-column class-name="status-col" label="Status" width="110" align="center">
+          <template slot-scope="scope">
+            <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+          <template slot-scope="scope">
+            <i class="el-icon-time" />
+            <span>{{ scope.row.display_time }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </template>
+  
+  <script>
+  import { getList } from '@/api/table'
+  
+  export default {
+    filters: {
+      statusFilter(status) {
+        const statusMap = {
+          published: 'success',
+          draft: 'gray',
+          deleted: 'danger'
+        }
+        return statusMap[status]
+      }
+    },
+    data() {
+      return {
+
+        list: null,
+        listLoading: true
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      fetchData() {
+        this.listLoading = true
+        getList().then(response => {
+          this.list = response.data.items
+          this.listLoading = false
+        })
+      }
+    }
+  }
+  </script>
+  
+<style>
+.boxes{
+    width: 18%;
+}
+</style>
